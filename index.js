7016797ce128cbf56e154328b8d29a1715d5750f
Daniel Banting <daniel.banting@ostmodern.co.uk>
2016-06-22 10:45:14 +0100

    new rules

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
  * Created by danielbanting on 01/04/2016.
  */
-
+const bestPracticesRules = require('./rules/best-practices');
 const stylelisticRules = require('./rules/stylelistic');
-const jsdocsRules = require('./rules/jsDocs');
+const jsdocsRules = require('./rules/js-docs');
 const miscRules = {
   'no-console': 'warn'
 };
 
 module.exports = {
-  rules: Object.assign({}, stylelisticRules, jsdocsRules, miscRules)
+  rules: Object.assign({}, stylelisticRules, jsdocsRules, miscRules, bestPracticesRules)
 };
---
+++ rules/best-practices.js
@@ -0,0 +1,9 @@
+/**
+ * Created by danielbanting on 22/06/2016.
+ */
+const rules = {
+  'no-eval': 'error',
+  'eqeqeq': ["error", "always"]
+};
+
+module.exports = rules;
