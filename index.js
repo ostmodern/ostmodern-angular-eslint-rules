030aa0328e4be499997f7d6e2adfdb19830d2e76
steoo <esseiproject@gmail.com>
2019-10-31 16:29:49 +0000

    removed miscRules

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
  */
 const bestPracticesRules = require('./rules/best-practices');
 const stylelisticRules = require('./rules/stylelistic');
-const miscRules = {
-  'no-console': 'warn'
-};
 
 module.exports = {
-  rules: Object.assign({}, stylelisticRules, miscRules, bestPracticesRules)
+  rules: Object.assign({}, stylelisticRules, bestPracticesRules)
 };
